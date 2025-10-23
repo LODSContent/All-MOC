31ed5f228ddd427140a395d302aa30cc9c7133dc
Yorik1971 <107193818+Yorik1971@users.noreply.github.com>
2025-10-23 12:10:15 -0400

    Update download-wwibacpac.sh
    updated the script to include the sqlpackage-linux package

--- Depth/download-wwibacpac.sh
+++ Depth/download-wwibacpac.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export DEBIAN_FRONTEND=noninteractive
+
 BACPAC_URL=$1
 SQL_SERVER=$2
 SQL_ADMIN=$3
@@ -13,6 +15,13 @@ curl -L -o "$BACPAC_PATH" "$BACPAC_URL"
 
 echo "Installing SQL package tools..."
 apt-get update -y
+apt-get install -y unzip curl libunwind8 libicu66
+# Download and install sqlpackage
+curl -L https://aka.ms/sqlpackage-linux -o sqlpackage.zip
+unzip sqlpackage.zip -d sqlpackage
+chmod +x sqlpackage/sqlpackage
+mv sqlpackage/sqlpackage /usr/local/bin/sqlpackage
+
 apt-get install -y curl apt-transport-https gnupg
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
